2b3e7710a73143b265a033bf382cb23fc6deb2ee
Aleksandr <aslyusarev@gmail.com>
2024-02-29 19:15:23 -0800

    Initial commit

---
+++ app/src/main/java/com/example/sp_android/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.sp_android
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var nameText : EditText
+    private lateinit var ageText : EditText
+    private lateinit var sf : SharedPreferences
+    private lateinit var editor : SharedPreferences.Editor
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        nameText = findViewById(R.id.etName)
+        ageText = findViewById(R.id.etAge)
+        sf = getSharedPreferences("my_sf", MODE_PRIVATE)
+        editor = sf.edit()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val name = nameText.text.toString()
+        val age = ageText.text.toString().toInt()
+        editor.apply{
+            putString("sf_name", name)
+            putInt("sf_age", age)
+            commit()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val name = sf.getString("sf_name", null)
+        val age = sf.getInt("sf_age", 0)
+
+        nameText.setText(name)
+        if(age!=0) {
+            ageText.setText(age.toString())
+        }
+    }
+}
